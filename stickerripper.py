df70f43034063dec432a79e2e553ab185ab4a47a
wiktorpp <56189344+wiktorpp@users.noreply.github.com>
2022-01-18 18:33:28 +0100

    Changed tabs to spaces

--- stickerripper.py
+++ stickerripper.py
@@ -5,11 +5,11 @@ null=None
 exec("a="+x)
 urls=[]
 for entry in a["log"]["entries"]:
-	urls.append(entry["request"]["url"].split("?")[0] + "?size=640")
+    urls.append(entry["request"]["url"].split("?")[0] + "?size=640")
 
 for url in urls:
-	open(
-		url.split("/")[-1].split("?size=640")[0], 'wb'
-	).write(
-		requests.get(url, allow_redirects=True).content
-	)
+    open(
+        url.split("/")[-1].split("?size=640")[0], 'wb'
+    ).write(
+        requests.get(url, allow_redirects=True).content
+    )
